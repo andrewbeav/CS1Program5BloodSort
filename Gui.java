480aca1eeeb9203e0d8ab595135dfcbb68af9e1f
Andrew Bevelhymer <andrew.bevelhymer@okstate.edu>
2016-10-08 19:35:39 -0500

    Add Gui class

---
+++ Gui.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame
+{
+  private JTextField fileNameText;
+  private JTextField sortFieldText;
+  private JButton sortButton;
+
+  public Gui()
+  {
+    this("Blood Donor Sorting Progam");
+    setLayout(new FlowLayout);
+
+    fileNameText = new JTextField(10);
+    sortFieldText = new JTextField(10);
+    sortButton = new JButton("sort");
+
+    add(fileNameText);
+    add(sortFieldText);
+    add(sortButton);
+  }
+}

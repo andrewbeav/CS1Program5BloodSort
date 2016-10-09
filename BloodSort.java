780ae32997c44ca2ed3340c4b6fd533aebb29f1e
Andrew Bevelhymer <andrew.bevelhymer@okstate.edu>
2016-10-08 21:32:05 -0500

    Update size of gui

--- BloodSort.java
+++ BloodSort.java
@@ -18,7 +18,7 @@ public class BloodSort
   {
     Gui gui = new Gui();
     gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setSize(300, 200);
+    gui.setSize(600, 500);
     gui.setVisible(true);
 
     // Print info
